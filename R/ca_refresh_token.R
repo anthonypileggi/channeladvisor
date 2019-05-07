0e5453a1a4021a2d0f0b617536ad7428bf1edf11
anthonypileggi <anthony@ifixit.com>
2019-05-07 11:57:54 -0400

    refresh access token

--- R/ca_refresh_token.R
+++ R/ca_refresh_token.R
@@ -1,19 +1,12 @@
 #' Refresh an access token
 #' @param access_token dev access token
 ca_refresh_token <- function() {
-  url <- "https://api.channeladvisor.com/"
-  response <- httr::POST(
-    "https://api.channeladvisor.com/oauth2/token",
-    httr::authenticate(Sys.getenv("CA_APP_ID"), Sys.getenv("CA_SECRET")),
-    query = list(
-      client_id = Sys.getenv("CA_APP_ID"),
-      grant_type = "soap",
-      developer_key = Sys.getenv("CA_DEV_KEY"),
-      password = Sys.getenv("CA_DEV_PASSWORD"),
-      account_id = Sys.getenv("CA_ACCOUNT_ID")
-      #grant_type = "refresh_token",
-      #refresh_token = Sys.getenv("CA_REFRESH_TOKEN")
-    )
-  )
-
+  url <- httr::modify_url("https://api.channeladvisor.com/", path = "oauth2/token")
+  auth <- httr::authenticate(Sys.getenv("CHANNELADVISOR_APP_ID"), Sys.getenv("CHANNELADVISOR_SECRET"))
+  query <- list(grant_type = "refresh_token", refresh_token = Sys.getenv("CHANNELADVISOR_REFRESH_TOKEN"))
+  response <- httr::POST(url, auth, body = query, encode = "form")
+  token <- httr::content(response)$access_token
+  Sys.setenv("CHANNELADVISOR_ACCESS_TOKEN" = token)
+  message("ChannelAdvisor access token has been refreshed!")
+  return(invisible(token))
 }
