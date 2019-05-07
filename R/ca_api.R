8e0a617f006317ab111fc0674952d047192682d9
anthonypileggi <anthony@ifixit.com>
2019-05-07 11:57:13 -0400

    update general api get call

--- R/ca_api.R
+++ R/ca_api.R
@@ -1,14 +1,19 @@
 #' Make a call to the ChannelAdvisor API
 #' @param endpoint API endpoint
 #' @param query named list with query to submit
-ca_api <- function(endpoint = "Products", query = NULL) {
+ca_api <- function(endpoint = "Products", ...) {
+
+  # prepare url
   url <- "https://api.channeladvisor.com/v1"
   q <- list(access_token = Sys.getenv("CHANNELADVISOR_ACCESS_TOKEN"))
-  if (!is.null(query))
-    q <- c(q, query)
-  response <- httr::GET(file.path(url, endpoint), query = q)
+  url <- httr::modify_url(url, path = file.path("v1", endpoint), query = q)
+
+  # get API response
+  response <- httr::GET(url, ...)
   x <- httr::content(response)
   out <- x$value
+
+  # paginate (if required)
   next_url <- x[["@odata.nextLink"]]
   i <- 1
   while (!is.null(next_url)) {
@@ -17,7 +22,8 @@ ca_api <- function(endpoint = "Products", query = NULL) {
     response <- httr::GET(next_url)
     x <- httr::content(response)
     out <- c(x$value, out)
-    next_url <- x[[3]]
+    next_url <- x[["@odata.nextLink"]]
   }
+
   return(out)
 }
