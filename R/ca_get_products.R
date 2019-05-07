d3c92938d856fe46a30c19be0b2780e6943e7097
anthonypileggi <anthony@ifixit.com>
2019-05-07 11:57:32 -0400

    parse response

--- R/ca_get_products.R
+++ R/ca_get_products.R
@@ -2,10 +2,20 @@
 #' @param sku product SKU
 #' @param access_token dev access token
 #' @export
-ca_get_products <- function(sku = NULL, access_token = Sys.getenv("CA_ACCESS_TOKEN")) {
+ca_get_products <- function(sku = NULL) {
+
+  # prepare filters
   q <- NULL
   if (!is.null(sku))
     q <- list("$filter" = paste0("Sku eq '", sku, "'"))
-  x <- ca_api("Products", query = q)
-  ca_parse_response(x)
+
+  # call api; parse response; convert fields to appropriate format
+  ca_api("Products", query = q) %>%
+    ca_parse_response() %>%
+    dplyr::mutate_at(
+      c("CreateDateUtc", "UpdateDateUtc", "QuantityUpdateDateUtc"),
+      ca_parse_datetime
+      )
 }
+
+
