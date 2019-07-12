ca67b02b654c445bf8f4d1135b542365eb3513ee
anthonypileggi <anthony@ifixit.com>
2019-07-11 20:25:45 -0400

    parse more datetimes

--- R/ca_parse_datetime.R
+++ R/ca_parse_datetime.R
@@ -1,6 +1,8 @@
 #' Parse datetime output from ChannelAdvisor API
 #' @param x datetime as character
 ca_parse_datetime <- function(x) {
-  x <- as.POSIXct(strptime(x, "%Y-%m-%dT%H:%M:%OSZ"), tz = "UTC")
-  lubridate::with_tz(x, tzone = Sys.timezone())
+  x1 <- as.POSIXct(strptime(x, "%Y-%m-%dT%H:%M:%OSZ"), tz = "UTC")
+  if (all(is.na(x1)))
+    x1 <- as.POSIXct(x, format = "%m/%d/%Y %H:%M:%S %p", tz = "UTC")
+  lubridate::with_tz(x1, tzone = Sys.timezone())
 }
