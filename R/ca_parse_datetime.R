534950e9c42dff6610d66dae5be16b1dcce27a95
anthonypileggi <anthony@ifixit.com>
2019-08-28 16:20:32 -0400

    account for no secconds

--- R/ca_parse_datetime.R
+++ R/ca_parse_datetime.R
@@ -4,5 +4,7 @@ ca_parse_datetime <- function(x) {
   x1 <- as.POSIXct(strptime(x, "%Y-%m-%dT%H:%M:%OSZ"), tz = "UTC")
   if (all(is.na(x1)))
     x1 <- as.POSIXct(x, format = "%m/%d/%Y %H:%M:%S %p", tz = "UTC")
+  if (all(is.na(x1)))
+    x1 <- as.POSIXct(x, format = "%m/%d/%Y %H:%M %p", tz = "UTC")
   lubridate::with_tz(x1, tzone = Sys.timezone())
 }
