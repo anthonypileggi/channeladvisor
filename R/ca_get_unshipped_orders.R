36ffcf1eb1df8a8f8630993daf528a5b6b1a20f3
anthonypileggi <anthony@ifixit.com>
2019-12-23 09:47:52 -0500

    add get unshipped orders function

---
+++ R/ca_get_unshipped_orders.R
@@ -0,0 +1,24 @@
+#' Get unshipped orders
+#' @importFrom magrittr "%>%"
+#' @export
+ca_get_unshipped_orders <- function() {
+
+  # prepare filters
+  q <- list(
+    "$orderby" = paste0("CreatedDateUtc desc"),
+    "$filter" = "ShippingStatus eq 'Unshipped'"
+    )
+
+  # call api
+  response <- ca_api("Orders", query = q)
+  if (length(response) == 0) {
+    message("No matching listings were found.")
+    return(NULL)
+  }
+
+  # parse response; convert fields to appropriate format
+  response %>%
+    purrr::map_df(~.x[c("ID", "SiteName", "CreatedDateUtc", "ShippingStatus")]) %>%
+    dplyr::mutate_at("CreatedDateUtc", channeladvisor:::ca_parse_datetime) %>%
+    dplyr::arrange(desc(CreatedDateUtc))
+}
