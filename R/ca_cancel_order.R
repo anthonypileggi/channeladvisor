e9986c24c293146d7ee5fcb8887f0cfa4e0d4589
anthonypileggi <apileggi20@gmail.com>
2021-09-17 11:42:02 -0400

    add multi-profile support for order cancellation

--- R/ca_cancel_order.R
+++ R/ca_cancel_order.R
@@ -1,27 +1,28 @@
 #' Cancel an order in ChannelAdvisor (note: do not notify marketplace)
 #' @param orderid ChannelAdvisor OrderID (character/scalar)
+#' @param account target Channeladvisor account/profile
 #' @export
-ca_cancel_order <- function(orderid) {
-  
-  channeladvisor:::ca_refresh_token()
-  
+ca_cancel_order <- function(orderid, account = "US") {
+
+  ca_set_account(account)
+
   # prepare url
   base_url <- "https://api.channeladvisor.com/v1"
   endpoint <- "Orders"
   endpoint <- paste0(endpoint, "(", orderid, ")/Adjust")
   q <- list(access_token = Sys.getenv("CHANNELADVISOR_ACCESS_TOKEN"))
   url <- httr::modify_url(base_url, path = file.path("v1", endpoint), query = q)
-  
+
   # payload
   data <- list(
     PreventSiteProcessing = TRUE
   )
-  
+
   # get API response
   response <- httr::POST(url, body = data, encode = "json")
-  
+
   if (httr::http_error(response))
     stop("Could not cancel order!")
-  
+
   return(response)
 }
